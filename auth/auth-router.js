c43207218f6d031af099a1889af39c4a9926d006
vy3191 <venkyafhs@gmail.com>
2020-04-05 14:52:40 -0400

    Added user modals

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const authDB = require('../database/dbConfig');
 
 router.post('/register', (req, res) => {
   // implement registration
+  const {username, password} = req.body;
+  if(!username) req.status(400).json({msg:'User name is missing'});
+  if(!password) req.status(400).json({msg:'Password is missing'});
+  if(!req.body) req.status(400).json({msg:'Something is missing'});
 });
 
 router.post('/login', (req, res) => {
---
+++ modals/user-modal.js
@@ -0,0 +1,24 @@
+const db = require('../database/dbConfig');
+const bcrypt = require('bcryptjs');
+
+function find() {
+  const users = db('users').select();
+  return users;
+}
+
+function findById(id) {
+  const user = db('users').where("id", id).first();
+  return user;
+
+
+function findBy(filter) {
+  const user =  db('users').where(filter).first();
+  return user
+};
+
+async function add(user) {
+  const user.password = bcrypt.hash(user.password, 16);
+  const [id] = await db('users').insert(user);
+  return await db('users').where("id", id).first();
+
+}
