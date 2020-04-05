a644cbb5ef98b56086b5819f63f4fadc5df8be0f
vy3191 <venkyafhs@gmail.com>
2020-04-05 17:45:56 -0400

    Completed authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,18 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.cookies.token;
+  if(!token) res.status(401).json({ you: 'shall not pass!' });
+
+  const JWT_SECRET = "STAY HOME, SAVE LIVES";
+
+  jwt.verify(token, JWT_SECRET, (err,decode) => {
+     if(err) res.status(401).json({ you: 'shall not pass!' });
+     req.token = decode;
+     next()
+  })
+
 };
