2859ae03eda5fdd0c92c9df7e96c0987e54afd09
vy3191 <venkyafhs@gmail.com>
2020-04-05 17:33:15 -0400

    Completed login router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,8 +20,28 @@ router.post('/register', async (req, res,next) => {
 
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res, next) => {
+  try {
+    const {username, password} = req.body;    
+    if(!username) res.status(400).json({msg:'User name is missing'});
+    if(!password) res.status(400).json({msg:'Password is missing'});
+    if(!req.body) res.status(400).json({msg:'Something is missing'});
+    const existingUser = await userModal.findBy(username);
+    const matchPassword = await bcrypt.compare(password, existingUser.password);
+    console.log('line 31', existingUser);
+    console.log('line 32', matchPassword);
+    // if(!existingUser || !matchPassword) res.status(401).json({msg:'Invalid credentials'});
+    //Implementing json web token
+    const payload = { userId: existingUser.id}
+    const secret = "STAY HOME, SAVE LIVES";
+    const token = jwt.sign(payload, secret);
+    res.setHeader('Set-Cookie', `token=${token}; path=/; httpOnly=true`);    
+    res.status(200).json({msg:`Welcome back ${username}`});
+  }catch(err) {
+    next(err);
+  }
+
+
 });
 
 module.exports = router;
