f956d9798776d2f0d7864ae2ae2de43bdebd0b89
vy3191 <venkyafhs@gmail.com>
2020-04-05 17:14:58 -0400

    Completed register route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const authDB = require('../database/dbConfig');
+const userModal = require('../modals/user-modal');
 
-router.post('/register', (req, res) => {
-  // implement registration
-  const {username, password} = req.body;
-  if(!username) req.status(400).json({msg:'User name is missing'});
-  if(!password) req.status(400).json({msg:'Password is missing'});
-  if(!req.body) req.status(400).json({msg:'Something is missing'});
+router.post('/register', async (req, res,next) => {  
+  try {
+    const {username, password} = req.body;    
+    if(!username) res.status(400).json({msg:'User name is missing'});
+    if(!password) res.status(400).json({msg:'Password is missing'});
+    if(!req.body) res.status(400).json({msg:'Something is missing'});
+    const existingUser = await userModal.findBy(username);
+    console.log(existingUser)
+    if(existingUser) res.status(409).json({msg:`User with username ${username} already exists`});
+    const newUser = await userModal.add(req.body);
+    res.status(201).json(newUser);
+  }catch(err) {
+    next(err);
+  }
+
 });
 
 router.post('/login', (req, res) => {
--- modals/user-modal.js
+++ modals/user-modal.js
@@ -9,19 +9,19 @@ function find() {
 function findById(id) {
   const user = db('users').where("id", id).first();
   return user;
-
+}
 
 function findBy(filter) {
-  const user =  db('users').where(filter).first();
-  return user
+  console.log('line15',filter);
+  const exists = db('users').where("username", filter).first();  
+  return exists;
 };
 
 async function add(user) {
-  const user.password = bcrypt.hash(user.password, 16);
+  user.password = await bcrypt.hash(user.password,16);
   const [id] = await db('users').insert(user);
-  return await db('users').where("id", id).first();
-};
-
-module.exports = {
-  find, findById, findBy, add
+  return await db('users').where('id', id).first();
 }
+
+
+module.exports = { find, findById, findBy, add };
